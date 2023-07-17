59acd2bf2f16f353408412c9ada4b18fd4de7087
Nahid <60399063+nahidbd11@users.noreply.github.com>
2023-07-17 23:56:17 +0600

    credentials auth working

--- app/_lib/authOptions.js
+++ app/_lib/authOptions.js
@@ -3,7 +3,7 @@ export const authOptions = {
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "Credentials",
+      name: "Credentials Nahid",
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
@@ -27,8 +27,13 @@ export const authOptions = {
         // const user = await res.json();
 
         // If no error and we have user data, return it
-
-        const user = { id: 1, name: "Nahid", token: "xxxaadfsdf1a1sd1f" };
+        console.log(credentials);
+        const user = {
+          id: 1,
+          name: "Nahid",
+          token: "xxxaadfsdf1a1sd1f",
+          email: "d@gmail.com",
+        };
         return user;
         // if (res.ok && user) {
         //   return user;
@@ -38,4 +43,20 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log(url);
+      console.log(baseUrl);
+      return "/home";
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token;
+    },
+  },
 };
--- app/page.js
+++ app/page.js
@@ -8,17 +8,31 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const router = useRouter();
+  const session = useSession();
+  console.log(session?.data?.user?.id);
   // const signIn = () => {
   //   console.log("clicked");
   //   router.push("/home");
   // };
+
+  const handleAuth = () => {
+    /*     signIn("credentials", {
+      callbackUrl: "http://localhost:3000/contact",
+      password: "ppp",
+      userName: "hiiid",
+    });
+
+ */
+
+    signIn();
+  };
 
   return (
     <main className="min-h-screen p-24">
       <h2 className="text-4xl">Hello world</h2>
       <button
         className="shadow-lg text-white px-4 py-2 rounded-lg bg-emerald-400 mt-2"
-        onClick={() => signIn()}
+        onClick={() => handleAuth()}
       >
         Login
       </button>
